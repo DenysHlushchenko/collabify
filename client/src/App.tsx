4f5e936762ae41aa3a6bb8fb2b589efbd103e932
eimunas <88548016+eimunas@users.noreply.github.com>
2026-02-12 18:50:39 +0100

    Initial project commit

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./modules/shared/components/ui/Button";
+
+function App() {
+    return (
+        <>
+            <h1 className="text-2xl">Collabify</h1>
+            <Button>Hello</Button>
+        </>
+    );
+}
+
+export default App;
