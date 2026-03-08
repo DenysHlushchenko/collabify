970f2d91fa35b265446c2fdb2b282a0f7740b9e0
eimunas <88548016+eimunas@users.noreply.github.com>
2026-03-08 11:38:34 +0100

    Add invalidate query for posts and tags

--- client/src/modules/chats/hooks/useChat.ts
+++ client/src/modules/chats/hooks/useChat.ts
@@ -1,6 +1,8 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { deleteChat, getAllChatsByUserId, getChatByUserId } from "../api/chat";
 import { toast } from "sonner";
+import type { FilterType } from "@/modules/shared/types/types";
+import { useSearchParams } from "react-router-dom";
 
 export const useChatsQuery = (userId?: number, edit?: boolean) => {
   return useQuery({
@@ -23,6 +25,11 @@ export const useChatQuery = (chatId: number, userId: number) => {
 
 export const useDeleteChatMutation = (chatId: number, userId: number) => {
   const queryClient = useQueryClient();
+  const [searchParams] = useSearchParams();
+
+  const filter = searchParams.get("filter") as FilterType;
+  const search = searchParams.get("search");
+
   return useMutation({
     mutationFn: () => deleteChat(chatId, userId),
 
@@ -34,6 +41,8 @@ export const useDeleteChatMutation = (chatId: number, userId: number) => {
 
       await queryClient.invalidateQueries({ queryKey: ["chats", userId] });
       await queryClient.invalidateQueries({ queryKey: ["chat", chatId] });
+      await queryClient.invalidateQueries({ queryKey: ["tags", "popular"] });
+      await queryClient.invalidateQueries({ queryKey: ["posts", filter, search] });
     },
 
     onError: (error) => {
