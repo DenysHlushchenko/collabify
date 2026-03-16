87a2c52746f7fd47412b218e4cfe6ced3ca762e1
Denys Hlushchenko <543499@student.saxion.nl>
2026-03-16 16:43:20 +0100

    Add prod url to cors, fix socket port

--- server/src/modules/messages/message.gateway.ts
+++ server/src/modules/messages/message.gateway.ts
@@ -24,7 +24,11 @@ interface AuthenticatedSocket extends Socket {
 
 @WebSocketGateway(5001, {
   cors: {
-    origin: ['http://localhost:5173', 'http://localhost'],
+    origin: [
+      'http://localhost:5173',
+      'http://localhost',
+      process.env.FRONTEND_URL,
+    ].filter(Boolean),
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   },
 })
